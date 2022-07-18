7851d169516946e840c085c916cd78f6cb5804ab
purnachandra rao obulasetty <purnachandra.obulasetty@gmail.com>
2022-07-18 12:01:29 +0100

    changes.

--- JPMCPlanetsChallengeTests/CoreData/PlanetsCoreDataServiceTest.swift
+++ JPMCPlanetsChallengeTests/CoreData/PlanetsCoreDataServiceTest.swift
@@ -9,10 +9,14 @@ class PlanetsCoreDataServiceTest: XCTestCase {
     
     override func setUp() {
         
+        // Get the Planets.json file path
         guard let filePath = Bundle(for: PlanetsListViewModelTest.self).url(forResource: "Planets", withExtension: "json"),let data = try? Data(contentsOf: filePath) else {
             return
         }
+        
         let decoder = JSONDecoder()
+        
+        // Using JSONDecoder convert date to resultsListModel.
         resultListModel = try? decoder.decode(PlanetResults.self, from: data)
         
     }
@@ -20,14 +24,21 @@ class PlanetsCoreDataServiceTest: XCTestCase {
     func testCoreDataRecordsInsertion() {
         
         let expectation = XCTestExpectation(description: "planets core data fetch")
+        
+        //Insert planet records using coreDataService.
         coreDataService.insertPlanetRecords(records: resultListModel.results!) { error in
             
+            // Insertion success with out an error
             if error == nil {
+                
+                // get planets records from core data
                 self.coreDataService.getPlanetRecords { planets in
                     
+                    // If planet records are available, the test case is success
                     if planets != nil && planets!.count > 0 {
                         expectation.fulfill()
                     }
+                    // insertion records failed.
                     else {
                         XCTFail("insert planet records failed")
                     }
